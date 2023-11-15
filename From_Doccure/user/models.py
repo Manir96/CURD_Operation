b91216c65864b9c49c129b164ed8dc2d7dee8c06
MANIRUZZAMAN <118476008+Manir96@users.noreply.github.com>
2023-11-15 14:03:41 +0600

    prediction template display:none

--- From_Doccure/user/models.py
+++ From_Doccure/user/models.py
@@ -2,4 +2,10 @@ from django.db import models
 
 # Create your models here.
 
-class user
+# class user_registration(models.Model):
+#     Full_Name = models.CharField(max_length=100)
+#     Email = models.EmailField()
+#     Mobile = models.IntegerField()
+#     description = models.TextField()
+#     brand = models.CharField(max_length=100)
+   
